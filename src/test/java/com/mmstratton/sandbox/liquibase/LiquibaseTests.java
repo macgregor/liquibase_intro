d2333c3e779ad414cb81ea72839df619363bd323
Matthew Stratton <mstratto@redhat.com>
2014-09-23 15:04:46 -0400

    fixing transaction issues caused by the datasoruce class I was using

--- src/test/java/com/mmstratton/sandbox/liquibase/LiquibaseTests.java
+++ src/test/java/com/mmstratton/sandbox/liquibase/LiquibaseTests.java
@@ -1,18 +1,34 @@
 package com.mmstratton.sandbox.liquibase;
 
+import javax.sql.DataSource;
+
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/app-context.xml")
+@TransactionConfiguration(defaultRollback=false) 
 public class LiquibaseTests {
 	static Logger log = Logger.getLogger(LiquibaseTests.class);
 	
+	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public void setDataSource(DataSource dataSource) {
+	      this.dataSource = dataSource;
+	      this.jdbcTemplate = new JdbcTemplate(this.dataSource);
+	   }
+	
+	
 	@Test
-	public void hello(){
-		log.info("Hello World");
+	public void testTableExists(){
+		jdbcTemplate.execute("insert into person(id, name) values(0, 'Matt')");		
 	}
 }
